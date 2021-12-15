ff620fd6c84ed81aa114bd963df87fee63bf2e38
dphamhuukhanh <dphamhuukhanh@gmail.com>
2021-12-15 15:15:52 +0700

    update preprocessing

--- Pre-processing/jsontocsv.py
+++ Pre-processing/jsontocsv.py
@@ -1,13 +1,15 @@
 import json
 import csv
+from xxx import preprocess
 
-with open("./resources/demo.json") as file:
+with open('./Pre-processing/resources/News_Category_Dataset_v2.json') as file:
     data = json.load(file)
-#News_Category_Dataset_v2.json
-fname = "output.csv"
 
-with open(fname, "w") as file:
+#News_Category_Dataset_v2.json
+fname = "Pre-processing/output.csv"
+
+with open(fname, "w", encoding="utf-8") as file:
     csv_file = csv.writer(file,lineterminator='\n')
-    csv_file.writerow(["CATEGORY","HEADLINE","AUTHORS","LINK", "SHORT_DESCRIPTION", "DATE"])
+    csv_file.writerow(["CATEGORY","HEADLINE"])
     for item in data["root"]:
-        csv_file.writerow([item['category'],item['headline'],item['authors'],item['link'], item['short_description'], item['date']])
+        csv_file.writerow([item['category'], preprocess(item['headline'])])
--- Pre-processing/xxx.py
+++ Pre-processing/xxx.py
@@ -1,5 +1,10 @@
+from os import remove
 import re
 import string
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+stopwords = set(stopwords.words('english'))
 
 def clean_header(text):
     text = re.sub(r'(From:\s+[^\n]+\n)', '', text)
@@ -7,7 +12,6 @@ def clean_header(text):
     text = re.sub(r'(([\sA-Za-z0-9\-]+)?[A|a]rchive-name:[^\n]+\n)', '', text)
     text = re.sub(r'(Last-modified:[^\n]+\n)', '', text)
     text = re.sub(r'(Version:[^\n]+\n)', '', text)
-
     return text
 
 def clean_text(text):
@@ -37,73 +41,24 @@ def clean_text(text):
     text = ' '.join([word for word in text.split() if len(word) > 1])
     return text.strip()
 
-start_time = time()
-df['cleaned_text'] = df['text'].apply(clean_text)
-print("Time to process data: " + str(time() - start_time) + 's')
-
-df['cleaned_text'].head()
-
-df['number_of_cleaned_words'] = df['cleaned_text'].apply(lambda x:len(str(x).split()))
-df.head()
-
-df['number_of_cleaned_words'].describe()
-
-# number of rows with text length = 0
-no_text = df[df['number_of_cleaned_words']==0]
-print(len(no_text))
-
-# drop these rows
-df.drop(no_text.index,inplace=True)
-
-df['tokens'] = df['cleaned_text'].apply(lambda x: x.split())
-df.head()
-
-# stopwords
-from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
-print(ENGLISH_STOP_WORDS)
-
-len(ENGLISH_STOP_WORDS)
-
-# removing stopwords
-stop_words = ENGLISH_STOP_WORDS
-
 def remove_stopwords(text):
-    words = [word for word in text if word not in stop_words]
-    return words 
-df['stopwords_remove_tokens'] = df['tokens'].apply(lambda x : remove_stopwords(x))
-df.head()
+    word_tokens = word_tokenize(text)
+    words_of_sentences = [word for word in word_tokens if not word.lower() in stopwords]
+    print(words_of_sentences)
+    return words_of_sentences
 
-from nltk.stem import WordNetLemmatizer
-lem = WordNetLemmatizer()
-
-def lem_word(x):
-    return [lem.lemmatize(w, pos="v") for w in x]
-
-df['lemmatized_text'] = df['stopwords_remove_tokens'].apply(lem_word)
-df.head()
 
 def combine_text(list_of_text):
     combined_text = ' '.join(list_of_text)
     return combined_text
 
-df['final_text'] = df['lemmatized_text'].apply(lambda x : combine_text(x))
-df.head()
+def preprocess(text):
+    txt = clean_header(text)
+    txt = clean_text(txt)
+    txt = remove_stopwords(txt)
+    txt = combine_text(txt)
+    return txt
 
-df['final_number_of_words'] = df['final_text'].apply(lambda x:len(str(x).split()))
-df.head()
-
-df['final_number_of_words'].describe()
-
-# number of rows with text length = 0
-no_text = df[df['final_number_of_words'] == 0]
-print(len(no_text))
-
-# drop these rows
-df.drop(no_text.index,inplace=True)
-
-plt.style.use('ggplot')
-plt.figure(figsize=(12,6))
-sns.distplot(df['final_number_of_words'], kde = False,color="red", bins=200)
-plt.title("Frequency distribution of number of words for each text extracted", size=20)
-
-df.to_csv(data_root_path + "data.csv", index=False)
+#demo
+#txt = 'This is a sample sentence, showing off the stop words filtration.'
+#print(preprocess(txt))
