b0e3491f5d9b7307950e39025f6b091aa2f5883c
dphamhuukhanh <dphamhuukhanh@gmail.com>
2021-12-15 15:42:21 +0700

    update preprocessing

--- Pre-processing/jsontocsv.py
+++ Pre-processing/jsontocsv.py
@@ -1,6 +1,6 @@
 import json
 import csv
-from xxx import preprocess
+from xxx import preprocess, combine_text
 
 with open('./Pre-processing/resources/News_Category_Dataset_v2.json') as file:
     data = json.load(file)
@@ -10,6 +10,12 @@ fname = "Pre-processing/output.csv"
 
 with open(fname, "w", encoding="utf-8") as file:
     csv_file = csv.writer(file,lineterminator='\n')
-    csv_file.writerow(["CATEGORY","HEADLINE"])
+    csv_file.writerow(["CATEGORY","CONTENT"])
     for item in data["root"]:
-        csv_file.writerow([item['category'], preprocess(item['headline'])])
+        content = []
+        content.append(item['headline'])
+        content.append(item['short_description'])
+        content[0] = preprocess(content[0])
+        content[1] = preprocess(content[1])
+        txt = combine_text(content)
+        csv_file.writerow([item['category'], txt])
